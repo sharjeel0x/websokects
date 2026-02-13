89ec8a89a51a080746c5ee7a5fa4b67ed18ba7de
sharj33l0x <sharj33lshahzad@gmail.com>
2026-02-14 01:11:37 +0500

    ghsfh

--- sportz/src/index.js
+++ sportz/src/index.js
@@ -1,4 +1,11 @@
-import express from 'express'
+import express, { json } from "express";
 
-const app = express()
+const app = express();
 
+const PORT = 8008;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log("server is running on Port", PORT);
+});
